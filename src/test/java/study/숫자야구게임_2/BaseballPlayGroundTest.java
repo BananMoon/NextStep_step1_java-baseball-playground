8da888a745f6da30f75ccbade7811cbc0e2637c8
BananMoon <younji1115@gmail.com>
2023-02-25 16:25:29 +0900

    refactor : 2. 숫자야구게임구현 - @ParameterizedTest 이용

--- src/test/java/study/숫자야구게임_2/BaseballPlayGroundTest.java
+++ src/test/java/study/숫자야구게임_2/BaseballPlayGroundTest.java
@@ -2,14 +2,27 @@ package study.숫자야구게임_2;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-// 모든 로직에 단위테스트 구현
+/**
+ * 했지만
+ *   Input 테스트 - getCompareResultTest()
+ *   Output 테스트 - System 설정을 통해 제어하지는 못함.
+ *   result를 print하는 로직을 메서드 추출한다면 Output 테스트 또한 가능하겠지만
+ *   본인은 guessAnswerUntilCorrect() 메서드를 실행하면서 true, false를 체크하였다.
+ *   - why? 올바른 값이 나와야 올바른 boolean 값을 반환하고, 그 사이에 Output 로직이 있기 때문에
+ *  boolean 값만 비교 체크하였다.
+ */
+
 class BaseballPlayGroundTest {
 
     @Test
@@ -19,66 +32,47 @@ class BaseballPlayGroundTest {
         assertEquals(answer.size(), 3);
     }
 
-    @Test
+    // input(String 배열)과 예측(Map<String, Integer>) 을 넣고, 결과와 예측값 비교
+    // @ParameterizedTest를 통해 선언 로직을 분리하고, 각 다른 인자들 모두 테스트할 수 있다.
+    @ParameterizedTest
+    @MethodSource("provideHashMapOfResult")
     @DisplayName("같은 수&같은 위치는 스트라이크, 같은 수가 있기만 하면 볼, 전혀 없으면 낫싱")
-    void getCompareResultTest() {
+    void getCompareResultTest(String[] input, Map<String, Integer> expected) {
         List<String> answer = List.of("7","1","3");
-        String[] input1 = new String[]{"2","1","6"};    // 1스트라이크
-
-        Map<String, Integer> expected = new HashMap<>();
-        expected.put("스트라이크", 1);
-        // compareResult static 해쉬맵은 테스트메서드에서 어떻게 사용하지?
-        Map<String, Integer> result = BaseballPlayGround.getCompareResult(answer, input1);
+        Map<String, Integer> result = BaseballPlayGround.getCompareResult(answer, input);
         assertAll(
                 () -> assertEquals(expected.size(), result.size()),
-                () -> assertEquals(expected.get("스트라이크"), result.get("스트라이크"))
-        );
-        assertNull(result.get("볼"));
-
-
-        String[] input2 = new String[]{"1","2","3"};    // 1볼 1스트라이크
-        Map<String, Integer> expected2 = new HashMap<>();
-        expected2.put("스트라이크", 1);
-        expected2.put("볼", 1);
-        Map<String, Integer> result2 = BaseballPlayGround.getCompareResult(answer, input2);
-        assertAll(
-                () -> assertEquals(expected2.size(), result2.size()),
-                () -> assertEquals(expected2.get("스트라이크"), result2.get("스트라이크")),
-                () -> assertEquals(expected2.get("볼"), result2.get("볼"))
+                () -> assertEquals(expected.get("스트라이크"), result.get("스트라이크")),
+                () -> assertEquals(expected.get("볼"), result.get("볼"))
         );
     }
-    // 작성 예정
-    // TODO System.in을 어떻게 할지?
-    // TODO 따로 메서드로 빼서 System.in을 세팅하면 가능한 듯함.
-    @Test
-    @DisplayName("정답을 맞췄을 때 끝나야 한다.")
-    void guessAnswerUntilCorrectTest() {
-        List<String> computer = List.of("7","1","3");
-        BaseballPlayGround.guessAnswerUntilCorrect(computer);
+    static Stream<Arguments> provideHashMapOfResult() {
+        return Stream.of(
+                Arguments.of(new String[]{"2","1","6"}, Map.of("스트라이크", 1)),
+                Arguments.of(new String[]{"1","2","3"}, new HashMap<String, Integer>() {{
+                    put("볼", 1);
+                    put("스트라이크",1);
+                }})
+        );
     }
 
-    @Test
     @DisplayName("낫싱, 3스트라이크 외에는 false이어야 한다.")
-    void correctCheckAndPrintResultTest() {
+    @ParameterizedTest
+    @MethodSource("provideCheckParam")
+    void correctCheckAndPrintResultTest(String[] input, boolean expect) {
         List<String> answer = List.of("7","1","3");
-        String[] oneStrikeInput = new String[]{"2","1","6"};    // 1스트라이크
-        String[] notSynchInput = new String[]{"2","4","6"};    // 낫싱
-        String[] strikeInput = new String[]{"7","1","3"};    // 스트라이크
-        String[] ballInput = new String[]{"1","4","6"};    // 볼
-        String[] ballAndStrikeInput = new String[]{"1","4","3"};    // 1볼 1스트라이크
 
-        boolean isCorrect = BaseballPlayGround.correctCheckAndPrintResult(answer, oneStrikeInput);
-        boolean isCorrect2 = BaseballPlayGround.correctCheckAndPrintResult(answer, notSynchInput);
-        boolean isCorrect3 = BaseballPlayGround.correctCheckAndPrintResult(answer, strikeInput);
-        boolean isCorrect4 = BaseballPlayGround.correctCheckAndPrintResult(answer, ballInput);
-        boolean isCorrect5 = BaseballPlayGround.correctCheckAndPrintResult(answer, ballAndStrikeInput);
+        boolean result = BaseballPlayGround.correctCheckAndPrintResult(answer, input);
+        assertEquals(expect, result);
+    }
 
-        assertAll(
-                () -> assertFalse(isCorrect),
-                () -> assertFalse(isCorrect2),
-                () -> assertFalse(isCorrect4),
-                () -> assertFalse(isCorrect5)
+    static Stream<Arguments> provideCheckParam() {
+        return Stream.of(
+                Arguments.of(new String[]{"2","1","6"}, false),
+                Arguments.of(new String[]{"2","4","6"}, false),
+                Arguments.of(new String[]{"7","1","3"}, true),
+                Arguments.of(new String[]{"1","4","6"}, false),
+                Arguments.of(new String[]{"1","4","3"}, false)
         );
-        assertTrue(isCorrect3);
     }
 }
